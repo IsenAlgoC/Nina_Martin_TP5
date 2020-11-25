c91e628c71bcf36010dd910f62860daaffb4503c
Nina MARTIN <nina.martin@isen.yncrea.fr>
2020-11-25 19:52:29 +0100

    Deuxième Dépot

--- TP5/Exo2/exo2.c
+++ TP5/Exo2/exo2.c
@@ -4,7 +4,11 @@
 
 
 #define TAILLEINITIALE 100
-
+typedef struct Tableau {
+	int* elt;// le tableau d’entiers
+	int size;// la taille de ce tableau d’entiers
+	int eltsCount;// le nombre d’éléments dans le tableau
+} TABLEAU;
 
 TABLEAU newArray() {//on initialise le tableau avec les variables de l'
 	int* elt = NULL;
